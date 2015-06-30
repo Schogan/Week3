9e65dfda3175ee8bb5e7f18a4cd7f444b2745f2e
Schogan <lrschanbacher@gmail.com>
2015-06-29 23:09:24 -0500

    finally

--- application.js
+++ application.js
@@ -1,13 +1,21 @@
 var toDoList = [];
 
+function loadToDoList(){
+	if(localStorage.todolist){
+		toDoList = JSON.parse(localStorage.todolist);
+		changeDisplay();
+	}
+}
+
 function changeDisplay(){
 	var list = document.getElementById("todolist");
-	list.innerHTML = "";
-	//This is the only place we actually change the screen.
+		list.innerHTML = "";
+		//This is the only place we actually change the screen.
 	for(var i=0; i<toDoList.length; i++){
 		list.innerHTML += '<div class="item">' + toDoList[i] + '(<a href="#" onclick="javascript:removeToDo(' + i + ')">X</a>)' +
 		'</div>';
 	}
+	localStorage.todolist = JSON.stringify(toDoList);
 }
 
 function removeToDo(itemToRemove){
@@ -25,8 +33,12 @@ function addToDo() {
 	
 	var tmpItem;
 	var newToDo = document.getElementById("todonew");
-	
 	tmpItem = newToDo.value;
+	
+	if(tmpItem === ""){
+		alert("You did not enter a valid entry");
+		return;
+	}
 	
 	toDoList.push(tmpItem);
 	
