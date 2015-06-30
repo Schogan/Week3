02436bbbabf6baa20f429c4cd804c9caabe4989f
Schogan <lrschanbacher@gmail.com>
2015-06-29 22:40:25 -0500

    working

--- application.js
+++ application.js
@@ -5,8 +5,20 @@ function changeDisplay(){
 	list.innerHTML = "";
 	//This is the only place we actually change the screen.
 	for(var i=0; i<toDoList.length; i++){
-		list.innerHTML += '<div class="item">' + toDoList[i] + '</div>';
+		list.innerHTML += '<div class="item">' + toDoList[i] + '(<a href="#" onclick="javascript:removeToDo(' + i + ')">X</a>)' +
+		'</div>';
 	}
+}
+
+function removeToDo(itemToRemove){
+	var newList = [];
+	for(var i=0;i<toDoList.length;i++){
+		if( i !== itemToRemove) {
+			newList.push(toDoList[i]);
+		}
+	}
+	toDoList = newList;
+	changeDisplay();
 }
 
 function addToDo() {
@@ -14,9 +26,7 @@ function addToDo() {
 	var tmpItem;
 	var newToDo = document.getElementById("todonew");
 	
-
 	tmpItem = newToDo.value;
-	
 	
 	toDoList.push(tmpItem);
 	
